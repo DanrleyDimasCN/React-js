8661d26437c95bec92ae4010afa52047b41fb0f8
Danrley Dimas Camargo Napoleão <danrleydimas@gmail.com>
2023-08-21 10:07:55 -0300

    update

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import FirstComponent from './components/FirstComponent'
+
+function App() {
+
+  return (
+   <div className="app">
+    <h1>Fundamentos do React</h1>
+    <FirstComponent/>
+   </div>
+  )
+}
+
+export default App
---
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,9 @@
+const FirstComponent = () => {
+    return (
+        <div>
+            <h2>Meu primeiro componente 2! </h2>
+        </div>
+    )
+}
+
+export default FirstComponent
